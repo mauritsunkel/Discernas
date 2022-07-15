afd0abf626095ff5db95e20f0988112525374b8c
Maurits Unkel <MauritsUnkel@gmail.com>
2022-07-15 15:01:15 +0200

    Changed to plotting with ggplot2

--- Seurat/R/Test Volcano plot.R
+++ Seurat/R/Test Volcano plot.R
@@ -1,10 +1,39 @@
+library(ggplot2)
+library(ggrepel)
 
-neg <- read.csv2(file = "C:/Users/mauri/Desktop/M/Erasmus MC PhD/Projects/Single Cell RNA Sequencing/Seurat/results/2021-11-02 13-06-19/integrated/BL_A + BL_C/DE_analysis/sample_markers/negative_pct1=BL_A-pct2=BL_C_p-val-adj=bonferroni.csv")
-pos <- read.csv2(file = "C:/Users/mauri/Desktop/M/Erasmus MC PhD/Projects/Single Cell RNA Sequencing/Seurat/results/2021-11-02 13-06-19/integrated/BL_A + BL_C/DE_analysis/sample_markers/positive_pct1=BL_A-pct2=BL_C_p-val-adj=bonferroni.csv")
+### USER PARAMETERS
+## A+C
+# file <- "C:/Users/mauri/Desktop/Single Cell RNA Sequencing/Seurat/results/Pipe_SCTv2_23-06 cluster_level_selection/integrated/BL_A + BL_C/after_selection_old/DE_analysis/sample_markers/pct1=BL_A-pct2=BL_C - (nz-)p-val st 0.05.csv"
+## N+C
+file <- "C:/Users/mauri/Desktop/Single Cell RNA Sequencing/Seurat/results/Pipe_SCTv2_23-06 cluster_level_selection/integrated/BL_N + BL_C/after_selection_old/DE_analysis/sample_markers/pct1=BL_C-pct2=BL_N - (nz-)p-val st 0.05.csv"
 
-data <- rbind(neg, pos)
+log2FC.threshold <- 1
+### END USER PARAMETERS
 
-plot(x = data$avg_log2FC, y = -log(data$p_val), xlab = "Average log-2 Fold Change", ylab = "p-val (Bonferroni adjusted)", ylim = c(min(-log(data$p_val)), round(max(-log(data$p_val_adj)[is.finite(-log(data$p_val_adj))]), -1)))
-abline(v = -.25, col = "red")
-abline(v = .25, col = "red")
-abline(h = -log(0.01), col = "red")
+
+# read data
+data <- read.csv2(file)
+
+# add up/down/no differential expression flags
+data$diffexpressed <- "NO"
+data$diffexpressed[data$avg_log2FC > log2FC.threshold & data$p_val_adj < 0.05] <- "UP"
+data$diffexpressed[data$avg_log2FC < -log2FC.threshold & data$p_val_adj < 0.05] <- "DOWN"
+
+# color for diffexpressed
+diffcolors <- c("blue", "red", "black")
+names(diffcolors) <- c("DOWN", "UP", "NO")
+
+# label up and down regulated genes
+data$delabel <- NA
+data$delabel[data$diffexpressed != "NO"] <- data$X[data$diffexpressed != "NO"]
+
+# plot data
+ggplot(data=data, aes(x=avg_log2FC, y=-log10(p_val_adj), col = diffexpressed, label = delabel)) +
+  geom_point() +
+  theme_minimal() +
+  geom_text_repel() +
+  scale_color_manual(values = diffcolors) +
+  geom_vline(xintercept=c(-log2FC.threshold, log2FC.threshold), col="red") +
+  geom_hline(yintercept=-log10(0.01), col="red") +
+  xlab("Average log2 Fold Change") +
+  ylab("-log10 P-val (Bonferroni adjusted)")
