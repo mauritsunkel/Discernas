6e206c2e5a32c6ee5012936f4a61b3c63485f74c
Maurits Unkel <MauritsUnkel@gmail.com>
2022-10-19 15:55:13 +0200

    IN DEV: Fixing separate ORA/GSEA analyes

--- Seurat/R/FGSEA.R
+++ Seurat/R/FGSEA.R
@@ -6,9 +6,13 @@ library(ggplot2)
 library(DOSE)
 
 
-
+### USER PARAMETERS
 set.seed(42)
 lfc_threshold <- 1
+### END USER PARAMETERS ###
+
+
+
 
 # load DEA result
 dea_result <- read.csv2("C:/Users/mauri/Desktop/Single Cell RNA Sequencing/Seurat/results/Pipe_SCTv2_23-06 cluster_level_selection/integrated/BL_A + BL_C/after_selection_old/DE_analysis/sample_markers/method=DESeq2-pct1=BL_A-pct2=BL_C - (nz-)p-val st 0.05.csv")
@@ -24,12 +28,6 @@ dea_result$X[excel_genes_ind] <- sapply(X = excel_genes, USE.NAMES = FALSE, FUN 
   paste0(toupper(ss[1]), as.integer(ss[2]))
 })
 
-
-# TODO test swapping signs
-# dea_result$avg_log2FC <- dea_result$avg_log2FC * -1
-# filter by log2 fold change threshold
-dea_result <- dea_result[dea_result$avg_log2FC > lfc_threshold,]
-## DEVNOTE: some tens of genes like SEPT10 became a date in excel, hard to read back in properly
 ## DEVNOTE: don't remove '.' and everything after because of matching ENSEMBL IDs with ENSEMBL symbols at origin
 ## DEVNOTE: Normally remove for proper Ensembl gene version matching to Entrez
 ## dea_result$X <- sub("\\.[^.]*$", "", dea_result$X)
@@ -37,17 +35,29 @@ dea_result <- dea_result[dea_result$avg_log2FC > lfc_threshold,]
 ensembl_genes_ids <- read.delim(file = "C:/Users/mauri/Desktop/Single Cell RNA Sequencing/Seurat/data/Ensembl genes.tsv",
                                 col.names = c("ensembl_gene_id", "ensembl_gene_name", "gene_type"))
 dea_result$ensembl_id <- ensembl_genes_ids$ensembl_gene_id[match(dea_result$X, ensembl_genes_ids$ensembl_gene_name)]
-entrez_ids <- mapIds(org.Hs.eg.db, keys=dea_result$ensembl_id, keytype="ENSEMBL", column="ENTREZID", multiVals="first")
+# map ENSEMBL to ENTREZIDs
+entrez_ids <- bitr(geneID = dea_result$ensembl_id, OrgDb = org.Hs.eg.db, fromType = "ENSEMBL", toType = "ENTREZID")
+## DEVNOTE: clusterProfiler::bitr mapped more non-NA than annotationDbi::mapIds
+# entrez_ids <- mapIds(org.Hs.eg.db, keys=dea_result$ensembl_id, keytype="ENSEMBL", column="ENTREZID", multiVals="first")
 # set length 0 values to NA, otherwise lose them while unlisting
-entrez_ids[lengths(entrez_ids) == 0] <- NA
-dea_result$entrez_id <- unname(unlist(entrez_ids))
+# entrez_ids[lengths(entrez_ids) == 0] <- NA
+
 # get DEG names and lfc
 deg_ids_lfc <- dea_result$avg_log2FC
-names(deg_ids_lfc) <- dea_result$entrez_id
+names(deg_ids_lfc) <- entrez_ids$ENTREZID
 # remove NA
 deg_ids_lfc <- deg_ids_lfc[!is.na(names(deg_ids_lfc))]
-# sort from high to low
+# sort decreasingly
 deg_ids_lfc <- sort(deg_ids_lfc, decreasing = TRUE)
+
+
+
+
+# TODO check which genes are not mapped?
+## check of those which are in head/tail 100 positions, because they would be very weighty in GSEA
+## and check specifically which ones are in abs(log2fc) > threshold positions
+
+## TODO perform gseXXX functions first
 
 
 
@@ -55,7 +65,7 @@ deg_ids_lfc <- sort(deg_ids_lfc, decreasing = TRUE)
 gene_ontology_types <- c("biological_process" = "BP")
 # gene_ontology_types <- c("biological_process" = "BP",
 #                          "molecular_function" = "MF",
-                         # "cellular_component" = "CC")
+# "cellular_component" = "CC")
 for (ind in 1:length(gene_ontology_types)) {
   # GOEA: provide a set of genes, compare against predefined gene set
   ## look for over representtion, but not by rank!
@@ -69,7 +79,7 @@ for (ind in 1:length(gene_ontology_types)) {
     pvalueCutoff = 1, # default: 0.05
     eps = 0, # default: 1e-10
     verbose = TRUE,
-    pAdjustMethod = "none", # default: "BH" (Benjamini-Hochberg)
+    pAdjustMethod = "none", # TODO try (always used by GuidoHooiberg fromclusterProfiler) the default: "BH" (Benjamini-Hochberg)
     seed = TRUE,
     by = "fgsea",
     scoreType = "pos"
@@ -87,62 +97,81 @@ for (ind in 1:length(gene_ontology_types)) {
   # ggsave(file = paste0("enrichmentPlot_geneOntology_", names(gene_ontology_types[ind]), ".png"), width = 30, height = 20, units = "cm")
 }
 
-# TODO create issue on clusterProfiler, mention how certain genelist with only pos and length 55 works
-## other with only pos and length only works for MF, not BP CC or ALL
-## with the error: Error in FUN(X[[i]], ...) : GSEA statistic is not defined when all genes are selected
-### on tracing back through package code the error is generated once length of 'stats' (geneList) vs 'selectedStats' (geneSets/(filtered) pathways) are compared
-#### is this a bug?
-#### Anyhow, how to overcome this?
-# based on the fsea::preparePathwaysAndStats() function I assume the filtered pathways become the same length as the geneList
-
-# should I post this here or to fgsea @ctlab Github?
-
-# clusterProfiler::gseGO --> clusterProfiler::GSEA_internal --> GSEA_internal <- DOSE:::GSEA_internal
-## https://github.com/YuLab-SMU/clusterProfiler/blob/f164715c2eb696542ba31141c964cfd407201c2b/R/gseAnalyzer.R
-# .GSEA <- GSEA_fgsea
-## https://github.com/YuLab-SMU/DOSE/blob/d94b63f50c3ac8c238800e4052576aa7f6af63ef/R/gsea.R
-# DOSE::fgsea
-## https://github.com/YuLab-SMU/DOSE/blob/d94b63f50c3ac8c238800e4052576aa7f6af63ef/R/gsea.R
-# fgsea::fgseaMultilevel
-## https://github.com/ctlab/fgsea/blob/c4fb22b2deae6d8808b90439ea3d7297dfc7b349/R/fgsea.R
-# fgsea::preparePathwaysAndStats & fgsea::calcGseaStat
-## https://github.com/ctlab/fgsea/blob/master/R/fgsea.R
-### https://github.com/ctlab/fgsea/blob/c4fb22b2deae6d8808b90439ea3d7297dfc7b349/R/fgsea.R preparePathwaysAndStats
-
-# TODO can switch x and size by passing 'count' and 'geneRatio' to one and the other
 
 
-# internl function used by clusterProfiler to select data from gse object
-df <- fortify(gse, showCategory = 10, split=".sign")
-df <- fortify(gse, showCategory = 10, split=NULL)
-df
+## TODO then subset with dplyr::filter(abs(log2fc) > threshold)
+# TODO test swapping signs
+# dea_result$avg_log2FC <- dea_result$avg_log2FC * -1
+# filter by log2 fold change threshold
+dea_result <- dea_result[dea_result$avg_log2FC > lfc_threshold,]
 
-
-gse <- df[df$.sign == "activated"]
+## TODO then run enrichXXX functions
 
 
 
 
 
 
+# clusterProfiler vignette: https://yulab-smu.top/biomedical-knowledge-mining-book/useful-utilities.html
+# general intro ORA vs GSEA: https://www.pathwaycommons.org/guide/primers/data_analysis/gsea/
+# Over-representation (enrichment) analysis (ORA): check DEG vs known biological relationship (via gene set)
+## will find genes where the difference is large and will fail where the difference is small, but evidenced in coordinated way in a set of related genes
+## limitations: 1) arbitrary user-defined inclusion criteria, 2) equal importance to each gene, 3) assume gene independance
+# Gene set enrichment analysis (GSEA): unordered collection of related genes (sets)
+## Pathway: type of gene set if functional relations are ignored
+## gene set enrichment analysis (GSEA): aggregates the per gene statistics across genes within a gene set
+### overcoming ORA limitation, by detecting situations where all genes in a predefined set change in a small but coordinated way
+## steps
+### local gene-level statistic: adj-p-val / log fold change
+### global gene-set statistics: weighted running sum by ranked-list gene prevalence, Enrichment Score (ES) = max score
+#### Normalized ES (NES): accounting for gene set size
+### significance testing & multiple testing correction
+#### Type 1 error: family of hypotheses, chance of false positive (significant p-val)
+##### False Discovery Rate (FDR): fraction of rejected null hypothesis (Type 1 error) that are true (false negatives)
+###### https://www.pathwaycommons.org/guide/primers/statistics/multiple_testing/
+###### https://www.nature.com/articles/nbt1209-1135
+###### https://jtd.amegroups.com/article/view/13609/11598
 
-enrichGO() # for subset analysis, and use full set for gseGO: https://github.com/YuLab-SMU/clusterProfiler/issues/509
+## OVER REPRESENTATION ANALYSIS (one-sided version of Fishers exact test, input DEG subsets)
+# enrichGO, enrichKEGG, enrichDO, enrichWP, enrichPathway, and the universal one enrichr
+## GO=Gene ontology, DOSE=Disease Ontology, WP=WikiPathways
+enrichGO()
+enrichKEGG()
+enrichWP()
+enrichPathway() # ReactomePA package
+enrichDO() # Disease Ontology
+enrichDGN() # Disease Gene Network
+enricher() # custom annotation, like MSigDB (https://www.gsea-msigdb.org/gsea/msigdb/human/collection_details.jsp#H)
 
-
+## GENE SET ENRICHMENTS ANALYSIS (full DEG)
+# gseGO, gseKEGG, gseWP, and the universal one GSEA
+gseGO()
 gseKEGG()
-gseMKEGG()
+gseWP()
+gsePathway() # ReactomePA package
+gseDO() # Disease Ontology
+gseDGN() # Disease Gene Network
+GSEA() # custom annotation, like MSigDB (https://www.gsea-msigdb.org/gsea/msigdb/human/collection_details.jsp#H)
 
 
-# GSEA works a bit differently.
-# For GSEA, you will provide ALL the genes in your analysis without a cutoff.
-# You will rank them and weigh them by some statistic (fold change, or log(p) for example).
-# The software will walk through from top to bottom, investigating a pathway.
-# If it runs into a gene in the pathway it'll boost your enrichment score,
-# if it doesn't it'll subtract some points.
-# The number of points it gives depends on the rank/weight,
-# such that the higher the rank and fold change, the bigger the boost in enrichment score.
-# At the end you're left with a single score that is high if the pathway genes are clustered at the top/bottom,
-# and low if they are randomly distributed throughout your list.
+## LEADING EDGE ANALYSIS
+# https://guangchuangyu.github.io/2016/07/leading-edge-analysis/
+
+
+
+## GENERAL
+goplot(gse)
+setReadable() # get symbols with IDs
+bitr() # convert gene IDs
+compareCluster() # try when have multiple gene lists from different experiments/conditions
+
+## VISUALIZATION
+enrichplot() # https://yulab-smu.top/biomedical-knowledge-mining-book/enrichplot.html#enrichplot
+browseKEGG()
+pathview()
+viewPathway() # reactome pathway analysis
+
+
 
 
 
@@ -154,6 +183,11 @@ gseMKEGG()
 
 
 
+## internal function used by clusterProfiler to select data from gse object
+# df <- fortify(gse, showCategory = 10, split=".sign")
+# df <- fortify(gse, showCategory = 10, split=NULL)
+# df
+# gse <- df[df$.sign == "activated"]
 
 
 
