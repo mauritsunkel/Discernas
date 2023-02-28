470c5173f03d4e7c01f5d413e2303b537f399cb9
Maurits Unkel <MauritsUnkel@gmail.com>
2023-02-28 14:02:11 +0100

    Moved R/utils/ to R/utils.R

--- R/utils/beep.R
+++
@@ -1,8 +0,0 @@
-
-beep <- function(n = 5) {
-  for(i in seq(n)){
-    system("rundll32 user32.dll, MessageBeep -1")
-    Sys.sleep(.5)
-  }
-  print(paste0("beeped at: ", format(Sys.time(), "%F %H-%M-%S")))
-}
